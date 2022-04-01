79446e9646e82357c027de0cb64c28f397710de1
Zach Birkbeck <zacherytb@gmail.com>
2022-04-01 13:17:38 -0400

    Start to threading
    multiple attempts were made. Issue with running PID (when press s it did not stop)

---
+++ ncount_threading.py
@@ -0,0 +1,361 @@
+#from curses import KEY_PPAGE
+#from xxlimited import Null
+import iqmotion as iq
+import time
+import math
+import keyboard
+import threading
+import concurrent.futures
+
+import numpy as np
+import matplotlib.pyplot as plt
+import turtle
+
+# Setup com ports (Subject to change based on where USB is plugged in)
+#com1 = iq.SerialCommunicator("/dev/tty.usbserial-AB63836A") #bottom motor # "COM12" #port 2 on macbook adapter
+#com2 = iq.SerialCommunicator("/dev/tty.usbserial-AB5X22Z3") #top motor #"COM13" #port 3 on macbook adapter
+com1 = iq.SerialCommunicator("COM12") #bottom motor # "COM12" #port 2 on macbook adapter
+com2 = iq.SerialCommunicator("COM13") #top motor #"COM13" #port 3 on macbook adapter 
+
+# com1 = iq.SerialCommunicator("/dev/cu.usbserial-14510")
+# com2 = iq.SerialCommunicator("/dev/cu.usbserial-14520")
+# com3 = iq.SerialCommunicator("/dev/cu.usbserial-14530")
+# com4 = iq.SerialCommunicator("/dev/cu.usbserial-14540")
+
+# Initialize motors as IQ objects
+vertiq1 = iq.Vertiq8108(com1, 0, firmware="cats")
+vertiq2 = iq.Vertiq8108(com2, 0, firmware="cats")
+# vertiq3 = iq.Vertiq2306(com3, 0, firmware="servo")
+# vertiq4 = iq.Vertiq2306(com4, 0, firmware="servo")
+vertiqs = [vertiq1, vertiq2]
+
+# Boolean Setups
+pause = True
+pid = True
+ncounter = True
+
+#Motor direction
+motorDir1 = 1
+motorDir2 = -1
+
+# Target Speed (rad/s)
+targetSpeed = 5
+targetSpeed1 = targetSpeed * motorDir1
+targetSpeed2 = targetSpeed * motorDir2
+#add input rpm to rad/s 
+
+# How often to update motors
+time_step = .0001
+
+# Angle offset
+testOffset = 90 #angle offset for test *****DEGREES*****
+testOffsetRAD = testOffset*3.14/180
+motorOff1 = 0 #motor1 bottom
+motorOff2 = .43+(testOffset*3.14/180) #motor2 top
+#motorOff3 = 0
+#motorOff4 = 0
+
+
+# P I D
+P = 1
+I = .5
+D = .1
+# 1,1,.1
+
+
+#make a cw PID and ccw PID make ccw position - time*target
+class PIDbot(object):
+    def __init__(self, KP, KI, KD, target, motorOff, motorDir):
+        self.kp = KP
+        self.ki = KI
+        self.kd = KD
+        self.target = target
+        self.error = 0
+        self.error_integral = 0
+        self.error_last = 0
+        self.error_derivative = 0
+        self.output = 0
+        self.prevTime = 0
+        self.currentTime = 0
+        self.motorOffset = motorOff
+        self.motorDir = motorDir
+        self.startTime = 0
+   
+    def compute(self, position):
+        self.currentTime = time.time()
+        self.error = ((self.currentTime-self.startTime) * self.target) - position + self.motorOffset
+        self.error_integral += self.error * (self.currentTime - self.prevTime)
+        self.error_derivative = (self.error - self.error_last) / (self.currentTime - self.prevTime)
+        self.error_last = self.error
+        self.output = self.kp*self.error + self.ki*self.error_integral + self.kd*self.error_derivative
+
+        self.prevTime = self.currentTime
+        #Limits
+        if self.output > 50:
+            self.output = 50
+        if self.output < -50:
+            self.output = -50
+        return self.output
+    
+class PIDtop(object):
+    def __init__(self, KP, KI, KD, target, motorOff, motorDir):
+        self.kp = KP
+        self.ki = KI
+        self.kd = KD
+        self.target = target
+        self.error = 0
+        self.error_integral = 0
+        self.error_last = 0
+        self.error_derivative = 0
+        self.output = 0
+        self.prevTime = 0
+        self.currentTime = 0
+        self.motorOffset = motorOff
+        self.motorDir = motorDir
+        self.startTime = 0
+   
+    def compute(self, position):
+        self.currentTime = time.time()
+        self.error = (-(self.currentTime-self.startTime) * self.target) + position + self.motorOffset
+        self.error_integral += self.error * (self.currentTime - self.prevTime)
+        self.error_derivative = (self.error - self.error_last) / (self.currentTime - self.prevTime)
+        self.error_last = self.error
+        self.output = self.kp*self.error + self.ki*self.error_integral + self.kd*self.error_derivative
+
+        self.prevTime = self.currentTime
+        #Limits
+        if self.output > 50:
+            self.output = 50
+        if self.output < -50:
+            self.output = -50
+        return self.output
+
+class Motor_bot(object):
+    def __init__(self, vertiq, KP, KI, KD, target, motorOff, motorDir):
+        global Motor_bot
+        self.velocity = 0
+        self.vertiq = vertiq
+        self.motorOffset = motorOff
+        self.PIDbot = PIDbot(KP, KI, KD, target * motorDir, motorOff,motorDir)
+
+    def set_velocity(self, velocity):
+        self.velocity = velocity
+   
+    def get_velocity(self):
+        return self.velocity
+    
+    def get_offset(self):
+        return self.motorOffset
+
+class Motor_top(object):
+    def __init__(self, vertiq, KP, KI, KD, target, motorOff, motorDir):
+        global Motor_top
+        self.velocity = 0
+        self.vertiq = vertiq
+        self.motorOffset = motorOff 
+        self.PIDtop = PIDtop(KP, KI, KD, target * motorDir, motorOff,motorDir)
+
+    def set_velocity(self, velocity):
+        self.velocity = velocity
+   
+    def get_velocity(self):
+        return self.velocity
+    
+    def get_offset(self):
+        return self.motorOffset
+
+def graph(x,y):
+    plt.plot(x, y)
+    plt.show()
+
+def find_phase(phase):
+    while(ncounter):
+        if phase % math.pi == 0:
+            n = phase/math.pi
+            return n
+        
+# class phaseCounter(threading.Thread):
+#     def __init__(self, phase):
+#         threading.Thread.__init__(self)
+#         self.phase = phase
+#         self.n = 0
+#         self.alive = False
+        
+#     def run(self):
+#         self.alive = True
+#         while self.alive:
+#             if self.phase % math.pi == 0:
+#                 self.n = self.phase/math.pi
+                
+#     def peek(self):
+#         return self.n
+    
+#     def finish(self):
+#         # stop the while loop in method run
+#         self.alive = False
+#         return self.value
+
+motor1 = Motor_bot(vertiq1, P, I, D, targetSpeed, motorOff1, motorDir1)
+motor2 = Motor_top(vertiq2, P, I, D, targetSpeed, motorOff2, motorDir2)
+motors = [motor1, motor2]#, motor3, motor4]
+
+
+# motor1.vertiq.set("multi_turn_angle_control", "ctrl_velocity", targetSpeed) 
+# motor1.vertiq.set("multi_turn_angle_control", "trajectory_angular_velocity", targetSpeed)
+# #motor1.vertiq.set("multi_turn_angle_control", "trajectory_duration", testDuration)
+# motor1.vertiq.set("multi_turn_angle_control", "trajectory_angular_displacement", 0) 
+
+# phasemot1 = threading.Thread(target=find_phase, args=[motor1.vertiq.get("multi_turn_angle_control", "ctrl_angle")])
+# phasemot2 = threading.Thread(target=find_phase, args=[motor2.vertiq.get("multi_turn_angle_control", "ctrl_angle")])
+# phasemot1.start();
+# phasemot2.start();
+
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    f1 = executor.submit(find_phase, motor1.vertiq.get("multi_turn_angle_control", "ctrl_angle"))
+    f2 = executor.submit(find_phase, motor2.vertiq.get("multi_turn_angle_control", "ctrl_angle"))
+    n1 = f1.result()
+    n2 = f2.result()
+    print("n1=",n1," n2=",n2)
+
+# phasemotTOP = phaseCounter(motor1.vertiq.get("multi_turn_angle_control", "ctrl_angle"))
+# phasemotBOT = phaseCounter(motor2.vertiq.get("multi_turn_angle_control", "ctrl_angle"))
+
+## start the count for n
+# phasemotTOP.start()
+# phasemotBOT.start()
+    
+#set max speed
+motor1.vertiq.set("multi_turn_angle_control", "angular_speed_max", targetSpeed)
+motor2.vertiq.set("multi_turn_angle_control", "angular_speed_max", targetSpeed)
+
+# for motor in motors:
+#     motor.vertiq.set("multi_turn_angle_control", "ctrl_velocity", 0)
+#     motor.vertiq.set("multi_turn_angle_control", "trajectory_angular_displacement", 0)
+#     motor.vertiq.set("multi_turn_angle_control", "trajectory_duration", 1)
+motor1.vertiq.set("multi_turn_angle_control", "ctrl_velocity", 0)
+motor1.vertiq.set("multi_turn_angle_control", "trajectory_angular_displacement", 0)
+motor1.vertiq.set("multi_turn_angle_control", "trajectory_duration", 1)
+motor2.vertiq.set("multi_turn_angle_control", "ctrl_velocity", 0)
+motor2.vertiq.set("multi_turn_angle_control", "trajectory_angular_displacement", 0)
+motor2.vertiq.set("multi_turn_angle_control", "trajectory_duration", 1)
+time.sleep(1.5)
+
+# Set initial speed of motors
+# for motor in motors:
+#     motor.vertiq.set("multi_turn_angle_control", "trajectory_angular_displacement", motor.motorOffset)
+#     motor.vertiq.set("multi_turn_angle_control", "trajectory_duration", 1)
+motor1.vertiq.set("multi_turn_angle_control", "trajectory_angular_displacement", motor1.motorOffset)
+motor1.vertiq.set("multi_turn_angle_control", "trajectory_duration", 1)
+motor2.vertiq.set("multi_turn_angle_control", "trajectory_angular_displacement", motor2.motorOffset)
+motor2.vertiq.set("multi_turn_angle_control", "trajectory_duration", 1)
+
+time.sleep(1.5)
+
+
+
+# MATPLOTLIB
+#poses = np.array([])
+#times = np.array([])
+   
+i = 0
+print("Now Beginning Ramp-Up\n")
+while i < targetSpeed: #Ramp up... wait for current == target speed
+    motor1.vertiq.set("multi_turn_angle_control", "ctrl_velocity", i)
+    motor2.vertiq.set("multi_turn_angle_control", "ctrl_velocity", -i)
+    i = i + 1
+    time.sleep(.05)
+    #print(motor1.vertiq.get("multi_turn_angle_control", "ctrl_velocity"))
+
+#Hold speed... wait for keyboard input to start PID (Before data aqu starts)
+value = input("Press B to begin test\n")
+if value == 'b' or value == 'B':
+    pause = False
+    
+    
+# Store start time of Program
+startTime = time.time()
+
+
+# for motor in motors:
+#     #motor.vertiq.set("multi_turn_angle_control", "ctrl_velocity", targetSpeed)
+#     motor.PID.startTime = startTime
+#     motor.PID.prevTime = startTime
+motor1.PIDbot.startTime = startTime
+motor1.PIDbot.prevTime = startTime
+motor2.PIDtop.startTime = startTime
+motor2.PIDtop.prevTime = startTime
+
+time.sleep(.005)
+#PID
+print("Now Beginning PID-Phase Control\n")
+print("Press S to stop test\n")
+# while PID: #PID...wait for keyboard input to stop phase control
+#     for index, motor in enumerate(motors):
+#         obsDisplacement = motor.vertiq.get("multi_turn_angle_control", "obs_angular_displacement")
+#         if obsDisplacement is not None:
+#             velocity = motor.PID.compute(obsDisplacement)
+#             motor.vertiq.set("multi_turn_angle_control", "ctrl_velocity", targetSpeed + velocity)
+
+# while PID: #PID...wait for keyboard input to stop phase control
+#     for index, motor in enumerate(motors):
+#         obsDisplacement = motor.vertiq.get("multi_turn_angle_control", "obs_angular_displacement")
+#         if obsDisplacement is not None:
+#             velocity = motor.PID.compute(obsDisplacement)
+#             motor.vertiq.set("multi_turn_angle_control", "ctrl_velocity", motor.get_offset()*targetSpeed + velocity)
+
+while pid:
+    obsDisplacement1 = motor1.vertiq.get("multi_turn_angle_control", "obs_angular_displacement")
+    if obsDisplacement1 is not None:
+        # velocity1 = motor1.PID.compute(obsDisplacement1)
+        # motor1.vertiq.set("multi_turn_angle_control", "ctrl_velocity", motorDir1*(targetSpeed + velocity1))
+        velocity1 = motor1.PIDbot.compute(obsDisplacement1)
+        motor1.vertiq.set("multi_turn_angle_control", "ctrl_velocity", targetSpeed + velocity1)
+    obsDisplacement2 = motor2.vertiq.get("multi_turn_angle_control", "obs_angular_displacement")
+    if obsDisplacement2 is not None:
+        # velocity2 = motor2.PID.compute(obsDisplacement2)
+        # motor2.vertiq.set("multi_turn_angle_control", "ctrl_velocity", motorDir2*(targetSpeed + velocity2))
+        velocity2 = motor2.PIDtop.compute(obsDisplacement2)
+        motor2.vertiq.set("multi_turn_angle_control", "ctrl_velocity", motorDir2*(targetSpeed + velocity2))
+            #print("Motor %s: Position Error: %s" % (index+1, motor.PID.error))
+
+            # """
+            # print(motor1.PID.error)
+           
+            # if (time.time()-startTime > 9):
+            #     poses = np.append(poses, velocity)
+            #     times = np.append(times, motor1.PID.currentTime)
+            # """
+    if keyboard.is_pressed('s'):  # if key 's' is pressed 
+                print("Test stopped\n")
+                pid = False
+                break  # finishing the loop
+
+
+#coast ... wait for vel/pos/everything to = 0 
+print("Now Beginning Coasting")
+for motor in motors:
+    motor.vertiq.set("multi_turn_angle_control", "ctrl_coast")
+time.sleep(2) 
+       
+motor1.vertiq.set("multi_turn_angle_control", "ctrl_velocity", 0)
+motor2.vertiq.set("multi_turn_angle_control", "ctrl_velocity", 0)
+time.sleep(2)
+
+# # Reset initial position to default from motors.  
+# print("Now Beginning Reset")
+# motor1.vertiq.set("multi_turn_angle_control", "trajectory_duration", 3)
+# motor1.vertiq.set("multi_turn_angle_control", "trajectory_angular_displacement", 0)
+# motor1.vertiq.set("multi_turn_angle_control", "angular_speed_max", 3)
+# motor2.vertiq.set("multi_turn_angle_control", "trajectory_duration", 3)
+# motor2.vertiq.set("multi_turn_angle_control", "trajectory_angular_displacement", motorOff2)
+# motor2.vertiq.set("multi_turn_angle_control", "angular_speed_max", 3)
+
+
+
+
+
+# # graph(times, poses)
+# #def resetVar():
+# #    global traj_pos
+# #    traj_pos 
+    
